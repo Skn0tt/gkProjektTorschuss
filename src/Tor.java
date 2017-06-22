09dcc1bdda9af203448f7e6335cdab35feb2fc83
Unknown <simoknott@gmail.com>
2017-06-22 09:59:41 +0200

    FInal 1.0
    BDK

--- src/Tor.java
+++ src/Tor.java
@@ -12,8 +12,8 @@ public class Tor {
 
     private double dicke = 10;
     private double tiefe = 100;
-    private double hoehe = 100;
-    private double breite = 200;
+    private double hoehe = 400;
+    private double breite = 1000;
 
     private GLVektor position;
 
@@ -23,10 +23,15 @@ public class Tor {
         up = new GLQuader(position.x, position.y + hoehe, position.z, breite, dicke, tiefe,"Bilder/Netz.jpg");
         left = new GLQuader(position.x - breite/2, position.y + hoehe/2, position.z, dicke, hoehe, tiefe, "Bilder/Netz.jpg");
         right = new GLQuader(position.x + breite/2, position.y + hoehe/2, position.z, dicke, hoehe, tiefe, "Bilder/Netz.jpg");
-        back = new GLQuader(position.x, position.y + hoehe/2, position.z + tiefe, breite, hoehe, dicke, "Bilder/Netz.jpg");
+        back = new GLQuader(position.x, position.y + hoehe/2, position.z + tiefe / 2, breite, hoehe, dicke, "Bilder/Netz.jpg");
     }
 
     public GLVektor getPosition(){
         return position;
+    }
+
+    public boolean beinhaltet(GLVektor pos){
+        if (pos.x > position.x - (breite / 2) && pos.x < position.x + (breite / 2)) return true;
+        else return false;
     }
 }
